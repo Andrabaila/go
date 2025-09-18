60c0e65693ddfa5d137ba38ef1c440a87b22c105
Yury Andrabaila <yandrabaila@gmail.com>
2025-09-18 09:02:09 +0200

    refactor: add check in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 // src/main.tsx
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+const root = document.getElementById("root");
+if (!root) throw new Error("Root element not found");
+
+createRoot(root).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
